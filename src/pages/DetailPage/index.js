8f665d784f2d4c9ee4c5f20c57a9730cea424408
sehyeongcho <shcho4701@gmail.com>
2024-04-15 17:18:14 +0900

    DetailPage 업데이트

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,12 @@
+/**
+ * 검색되는 영화의 포스터를 클릭했을 때 보여주기 위한 영화 상세 페이지 파일입니다.
+ */
+
 import axios from '../../api/axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import '../../components/MovieModal/MovieModal.css'
+import './DetailPage.css'
 
 const DetailPage = () => {
   let { movieId } = useParams()
@@ -20,12 +25,27 @@ const DetailPage = () => {
   if (!movie) return null
 
   return (
-    <section>
+    <section className="detail-container">
       <img
         className="modal__poster-img"
         src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
         alt="modal__poster-img"
       />
+      <div className="detail-contents">
+        <h1>{movie.title}</h1>
+        <p>평균 {movie.vote_average}점</p>
+        <p>{movie.release_date} 개봉</p>
+        <p>{movie.runtime}분</p>
+        <ul>
+          {movie.genres?.map((genre) => {
+            return (
+              <li className="genre" key={genre.id}>
+                {genre.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </section>
   )
 }
